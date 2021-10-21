e9a8c507575f0cf44cd7273f3cb8cb01ee2e789a
Jacob Kravits <kravitsjacob@gmail.com>
2021-10-20 22:29:39 -0600

    Created default app layout

--- utils.py
+++ utils.py
@@ -1,8 +1,26 @@
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output, State
 
 
+def create_dashboard(group_labels_with_columns, group_values):
+    app = dash.Dash(__name__)
 
+    app.layout = html.Div(
+        [
+            html.Label(
+                [
+                    'Select Correlation Threshold',
+                    dcc.Input(
+                        id='cor_threshold',
+                        type='number'
+                    )
+                ]
+            ),
+            html.Button(id='update_button', n_clicks=0, children='Update Plot'),
+            html.Div(html.Iframe(id='parallel', style={'width': '100%', 'height': '1080px'}))
+        ]
+    )
 
-def create_dashboard(group_labels, group_values):
-
-
-    return 0
+    return app
