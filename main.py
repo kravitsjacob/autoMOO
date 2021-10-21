054b55449e70aa61c9a08f654dacaba8da2e4e29
Jacob Kravits <kravitsjacob@gmail.com>
2021-10-20 18:53:33 -0600

    Create example inputs

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 
+import pandas as pd
+import utils
+
 
 def main():
 
+    # TODO create true input function
+    df = pd.read_csv('example_datasets/water_energy_sensitivity.csv')
+    group_labels_with_columns = dict(zip(df.columns.values, df.columns.values))  # Assumes each column is group
+    group_values = df.to_numpy()
+
+    # Create dashboard
+    utils.create_dashboard(group_labels_with_columns, group_values)
 
     return 0
 
