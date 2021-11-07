f791e8ff87b77649110340ff4171bc7deeda25a8
Jacob Kravits <kravitsjacob@gmail.com>
2021-11-06 20:37:17 -0600

    Create outline for unit testing utils_testing.py

---
+++ utilts_testing.py
@@ -0,0 +1,17 @@
+"""Testing library for analysis.py"""
+
+import unittest
+
+import utils
+
+
+class AnalysisLib(unittest.TestCase):
+
+    def test_create_dashboard_default(self):
+
+        a = 1
+
+
+
+if __name__ == '__main__':
+    unittest.main()
