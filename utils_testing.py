b90a90f2b0f1fb34d9094cc4c822c269117fa6a4
Jacob Kravits <kravitsjacob@gmail.com>
2021-12-07 15:09:32 -0700

    Updated data types to be list of dictionaries for unit tests for group_columns

--- utils_testing.py
+++ utils_testing.py
@@ -11,6 +11,9 @@ import utils
 class AnalysisLib(unittest.TestCase):
 
     def test_file_reader_import(self):
+        """
+        Basic test for the file reader
+        """
         # Setup
         test_file = open('test.txt', 'w')
         test_file.write("A,B,C\n1,0.1,'1'\n 2,0.2,'2'\n3,0.3,'3'")
@@ -33,19 +36,15 @@ class AnalysisLib(unittest.TestCase):
         Default test to see if app is created with basic inputs
         """
         # Setup
-        df = pd.DataFrame({'A': [0, 1], 'B': [2, 1]})
-        cor_matrix = df.corr().to_numpy()
-        group_labels_with_columns = dict(
-            zip(df.columns.values, df.columns.values)
-        )
-        group_values = df.to_numpy()
+        data = [
+            {'A': 0, 'B': 0},
+            {'A': 1, 'B': 1},
+        ]
 
         # Create AutoMOO app (assertion assumed no error raised)
         utils.create_dashboard(
-            group_labels_with_columns,
-            group_values,
-            cor_matrix,
-            cor_fig=go.Figure()
+            data=data,
+            cor_colormap='RdBu'
         )
 
     def test_column_grouping_all_grouping(self):
@@ -55,7 +54,6 @@ class AnalysisLib(unittest.TestCase):
             {'A': 1, 'B': 1, 'C': 1},
             {'A': 2, 'B': 2, 'C': 2}
         ]
-
         cors, _ = utils.correlation_matrix(data, None)
         cor_threshold = 0.5
 
@@ -68,7 +66,11 @@ class AnalysisLib(unittest.TestCase):
 
         # Test
         group_labels_with_columns_expect = {'Group 1': ['A', 'B', 'C']}
-        data_grouped_expect = {'Group 1': [0, 1, 2]}
+        data_grouped_expect = [
+            {'Group 1': 0},
+            {'Group 1': 1},
+            {'Group 1': 2}
+        ]
 
         self.assertEqual(
             group_labels_with_columns,
@@ -80,29 +82,21 @@ class AnalysisLib(unittest.TestCase):
             data_grouped_expect
         )
 
-
     def test_column_grouping_no_grouping(self):
         # Setup
-        df_test = pd.DataFrame(
-            {
-                'A': [0, 1, 2],
-                'B': [2, 1, 0],
-                'C': [1, 1, 2]
-            }
-        )
-        cor_matrix, cor_matrix_visual = utils.correlation_matrix(df_test, None)
-        data = df_test.to_numpy()
-        column_labels = dict(
-            zip(df_test.columns.values, df_test.columns.values)
-        )
+        data = [
+            {'A': 0, 'B': 2, 'C': 1},
+            {'A': 1, 'B': 1, 'C': 1},
+            {'A': 2, 'B': 0, 'C': 2}
+        ]
+        cors, _ = utils.correlation_matrix(data, None)
         cor_threshold = 0.9
 
         # Run
-        group_labels_with_columns, group_values = utils.group_columns(
-            column_labels,
-            data,
-            cor_threshold,
-            cor_matrix
+        data_grouped, group_labels_with_columns = utils.group_columns(
+            data=data,
+            cors=cors,
+            cor_threshold=cor_threshold,
         )
 
         # Test
@@ -111,43 +105,37 @@ class AnalysisLib(unittest.TestCase):
             'Group 2': ['B'],
             'Group 3': ['C']
         }
-        group_values_expect = [[0, 1, 2], [2, 1, 0], [1, 1, 2]]
-        group_values_modified = [cols.tolist() for cols in group_values]
-
-        for i in range(len(group_values_modified)):
-            self.assertEqual(
-                group_values_modified,
-                group_values_expect
-            )
+        data_grouped_expect = [
+            {'Group 1': 0, 'Group 2': 2, 'Group 3': 1},
+            {'Group 1': 1, 'Group 2': 1, 'Group 3': 1},
+            {'Group 1': 2, 'Group 2': 0, 'Group 3': 2}
+        ]
 
         self.assertEqual(
             group_labels_with_columns,
             group_labels_with_columns_expect
         )
 
+        self.assertEqual(
+            data_grouped,
+            data_grouped_expect
+        )
+
     def test_column_grouping_basic_grouping(self):
         # Setup
-        df_test = pd.DataFrame(
-            {
-                'A': [0, 1, 2],
-                'B': [0, 1, 1.5],
-                'C': [2, 1, 0],
-                'D': [1.5, 1, 0],
-            }
-        )
-        cor_matrix, cor_matrix_visual = utils.correlation_matrix(df_test, None)
-        data = df_test.to_numpy()
-        column_labels = dict(
-            zip(df_test.columns.values, df_test.columns.values)
-        )
+        data = [
+            {'A': 0, 'B': 0.0, 'C': 2, 'D': 1.5},
+            {'A': 1, 'B': 1.0, 'C': 1, 'D': 1.0},
+            {'A': 2, 'B': 1.5, 'C': 0, 'D': 0.0}
+        ]
+        cors, _ = utils.correlation_matrix(data, None)
         cor_threshold = 0.9
 
         # Run
-        group_labels_with_columns, group_values = utils.group_columns(
-            column_labels,
-            data,
-            cor_threshold,
-            cor_matrix
+        data_grouped, group_labels_with_columns = utils.group_columns(
+            data=data,
+            cors=cors,
+            cor_threshold=cor_threshold,
         )
 
         # Test
@@ -155,19 +143,19 @@ class AnalysisLib(unittest.TestCase):
             'Group 1': ['A', 'B'],
             'Group 2': ['C', 'D']
         }
-        group_values_modified = [cols.tolist() for cols in group_values]
-        group_values_expect = [[0, 1, 2], [2, 1, 0]]
-
+        data_grouped_expect = [
+            {'Group 1': 0, 'Group 2': 2},
+            {'Group 1': 1, 'Group 2': 1},
+            {'Group 1': 2, 'Group 2': 0}
+        ]
         self.assertEqual(
             group_labels_with_columns,
             group_labels_with_columns_expect
         )
-
-        for i in range(len(group_values_modified)):
-            self.assertEqual(
-                group_values_modified,
-                group_values_expect
-            )
+        self.assertEqual(
+            data_grouped,
+            data_grouped_expect
+        )
 
 
 if __name__ == '__main__':
